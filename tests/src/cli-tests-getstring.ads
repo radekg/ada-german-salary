d29b26bb5109bd23f62cbbb8e6d26961ac3770ea
radekg <radek@gruchalski.com>
2022-08-28 01:29:16 +0200

    Clarify the TC idea in tests.

--- tests/src/cli-tests-getstring.ads
+++ tests/src/cli-tests-getstring.ads
@@ -4,11 +4,15 @@ with AUnit.Test_Cases; use AUnit.Test_Cases;
 package Cli.Tests.Getstring is
 
     type TC is new Test_Cases.Test_Case with record
-        A : Boolean := false;
+        null;
+        -- I can declare properties here like this:
+        -- https://github.com/yaml/AdaYaml/blob/develop/test/src/yaml-parser-event_test.ads#L13
+        -- which I can then initialize in Set_Up.
     end record;
 
-    overriding procedure Register_Tests (T : in out TC);
     overriding procedure Set_Up (T : in out TC);
+
+    overriding procedure Register_Tests (T : in out TC);
 
     function Name (T : TC) return Message_String;
 
